44fd8904932a3fa49121b26957fc2108af0edfb4
yiurbrazkaMGB <92884720+yiurbrazkaMGB@users.noreply.github.com>
2021-10-20 14:22:08 -0400

    Add files via upload
    practica primer corte

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            int a = 3;
+
+            Console.WriteLine("a"+ a);
+
+            int b = 7; 
+
+            Console.WriteLine("b"+ b);
+
+            int c = 9;
+                                 
+            Console.WriteLine("c"+ c);
+            
+            //aabc
+            Console.Write(a);
+            Console.Write(a);
+            Console.Write(b);
+            Console.WriteLine(c);
+
+            Console.WriteLine("______");
+
+            //bca
+            Console.Write(b);
+            Console.Write(c);
+            Console.WriteLine(a);
+
+            Console.WriteLine("______");
+
+            //bcbc
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(b);
+            Console.WriteLine(c);
+
+            Console.WriteLine("______");
+
+
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
